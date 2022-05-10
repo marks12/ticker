3f19162436adc39dcd3e253f4d985cd7c2d2f909
Tsv <tsarevnikov.vu@sibvaleo.com>
2022-05-10 21:23:20 +0700

    first verstion

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # ticker
 Переодический запуск функций по заданному расписанию
+
+## Использование
+
+```golang
+
+// Удаляем устаревшие данные
+func clearOldData(ctx context.Context) (err error) {
+
+    err = common.Ticker(ctx, TickerClearCode, func(tick ticker.Tick) (err error) {
+        err = clearOldData(ctx)
+        return err
+    }, time.Hour * 24, 0)
+    
+    return
+}
+	
+```
---
+++ ticker.go
@@ -0,0 +1,104 @@
+package ticker
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+)
+
+// Tick - DTO user operation tick data
+type Tick struct {
+	// function context
+	Ctx     context.Context
+	// user operation code
+	Code    string
+	// user operation tick period
+	Period  time.Duration
+	// current tick counter
+	Counter int
+}
+
+// Единственный инстанс, хранящий список запущенных функций для однократного запуска
+var tickerInstance *sync.Map
+var once sync.Once
+
+func GetInstance() *sync.Map {
+	once.Do(func() {
+		tickerInstance = &sync.Map{}
+	})
+
+	return tickerInstance
+}
+
+// Ticker - хелпер для запуска функции с некоторой периодичностью
+// times - количество циклов которые отработает тикер
+// Если times=0 тикер работает бесконечно
+func Ticker(ctx context.Context, code string, function func(tick Tick) error, period time.Duration, times int) (err error) {
+
+	inst := GetInstance()
+
+	if existsPeriod, ok := inst.Load(code); ok == true {
+		err = fmt.Errorf("Метод с кодом %s уже запущен. Вызывается с периодом %+v ", code, existsPeriod)
+		return
+	} else {
+		inst.Store(code, period)
+	}
+
+	var count = 0
+
+	// канал управления запуском итераций
+	ch := make(chan int)
+
+	go func() {
+
+		// бесконечно циклим
+		for {
+
+			// выбираем входящее данные по каналам
+			select {
+
+			// если есть данные из управляющего канала, получаем их и запускаем целевую функцию
+			case <-ch:
+
+				go func() {
+
+					time.Sleep(period)
+
+					// даже если прошел таймаут
+					// смотрим не пришел ли нам cancel
+					// если пришел, то никаких выполнений, на выход
+					select {
+					case <-ctx.Done():
+						fmt.Printf("Cancel func %+v\n", code)
+						return
+					default:
+						_ = function(Tick{
+							Ctx:     ctx,
+							Code:    code,
+							Period:  period,
+							Counter: count,
+						})
+
+						count += 1
+						ch <- 1
+					}
+				}()
+
+				if count > 0 && count == times {
+					return
+				}
+
+			//	если вдруг приходит отмена, немедленно сворачиваемся не дожидаясь следующей итерации
+			case <-ctx.Done():
+				fmt.Printf("Cancel func %+v\n", code)
+				return
+			}
+		}
+	}()
+
+	// инициируем запуск тикера
+	ch <- 1
+
+	return
+}
